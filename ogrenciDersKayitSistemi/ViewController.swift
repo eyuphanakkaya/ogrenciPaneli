cd8b341d4c957fde092dc62852873099800f651b
eyuphanakkaya <akkayaeyupp@gmail.com>
2023-03-14 10:26:53 +0300

    Initial Commit

---
+++ ogrenciDersKayitSistemi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ogrenciDersKayitSistemi
+//
+//  Created by Eyüphan Akkaya on 14.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
